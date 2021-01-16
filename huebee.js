6c23f0ddf9fae4eecedb81af49831d9c8f1d5056
Dave DeSandro <desandrocodes@gmail.com>
2021-01-15 19:45:51 -0500

    🛠 refactor #27

--- huebee.js
+++ huebee.js
@@ -575,22 +575,15 @@ Huebee.data = function( elem ) {
 
 // -------------------------- getSwatch -------------------------- //
 
-// proxy canvas used to check colors
 var proxyCtx;
 
-function initProxyCtxIfNeeded() {
-  // return early when proxy canvas is already initialized.
-  if ( proxyCtx ) {
-    return;
-  }
-
-  var proxyCanvas = document.createElement('canvas');
-  proxyCanvas.width = proxyCanvas.height = 1;
-  proxyCtx = proxyCanvas.getContext('2d');
-}
-
 function getSwatch( color ) {
-  initProxyCtxIfNeeded();
+  if ( !proxyCtx ) {
+    // init proxyCtx when needed. #27
+    var proxyCanvas = document.createElement('canvas');
+    proxyCanvas.width = proxyCanvas.height = 1;
+    proxyCtx = proxyCanvas.getContext('2d');
+  }
   // check that color value is valid
   proxyCtx.clearRect( 0, 0, 1, 1 );
   proxyCtx.fillStyle = '#010203'; // reset value
